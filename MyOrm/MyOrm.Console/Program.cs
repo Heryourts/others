1d87af127bb76b673fc7d33314e45bf7f831c1d0
Sergio <52083428+Heryourts@users.noreply.github.com>
2022-06-27 01:03:37 -0500

    Add files via upload

---
+++ MyOrm/MyOrm.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MyOrm;
+
+using (var context = new MyOrmContext())
+{
+    var bankAccount = new BankAccount { AccountNumber = 100, Owner = "Jose Perez" };
+    context.BankAccount.Add(bankAccount);    
+
+    for (int i = 0; i < 10; i++)
+    {
+        context.Transaction.Add(new Transaction { Amount = 250, 
+                                                  BankAccount = bankAccount, 
+                                                  Date = DateTime.Now, 
+                                                  Note = "Deposito", 
+                                                  Type = TransactionType.Deposit });
+    }
+
+    context.SaveChanges();
+}
+
+
---
+++ MyOrm/MyOrm/BankAccount.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyOrm
+{   
+    public class BankAccount
+    {
+        [Key]
+        public int Id { get; set; }
+        public int AccountNumber { get; set; }
+        public string Owner { get; set; }
+        public List<Transaction> Transactions { get; set; }
+    }
+}
